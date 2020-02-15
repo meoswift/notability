c8b7308dd72ca2e97deabcb0d1ea574d335c02d4
Linh Tran <linh.tran@tufts.edu>
2020-02-15 04:12:23 -0500

    updated functionality for google-keep clone

---
+++ public/script.js
@@ -0,0 +1,124 @@
+/* 
+ * guggle keep 
+ * author: linh tran
+ * 
+ * script.js
+ * 
+ * purpose: web application replicating google keep :) 
+ *
+ */
+
+const logo = document.querySelector("#logo")
+
+/* Select sidebar elements */
+const sidebarList = document.querySelectorAll("#sidebar button")
+const sidebar     = document.querySelector("#sidebar");
+
+/* Select notes elements */
+const noteForm = document.getElementById("note-form")
+const noteTemplate = document.getElementById("note-template")
+const noteTitle = document.querySelector('[data-new-title]')
+const noteContent = document.querySelector('[data-new-note]')
+const notesContainer = document.getElementById("notes-area")
+
+/* Use local storage to store which tab the user is on, default is notes */
+const LOCAL_STORAGE_SELECTED_TAB_ID_KEY = 'tab.selectedId'
+let selectedTabId = localStorage.getItem(LOCAL_STORAGE_SELECTED_TAB_ID_KEY) || 
+                    'notes'
+
+const LOCAL_STORAGE_SELECTED_NOTES_ID_KEY = 'notes.id'
+let notesList = JSON.parse(localStorage.getItem(LOCAL_STORAGE_SELECTED_NOTES_ID_KEY)) || []
+console.log(notesList);
+
+/* When an item on the side bar is clicked on, direct the user to that tab */
+sidebar.addEventListener('click', e => {
+  if (e.target.tagName.toLowerCase() === 'button') {
+      selectedTabId = e.target.id   
+  }  
+  save()
+  render()
+})
+
+/* If user clickes the logo, redirects to Notes tab */
+logo.addEventListener('click', () => {
+    selectedTabId = 'notes'
+    save()
+    render()
+})
+
+/* function to get values from form and add a note to noteList */
+noteForm.addEventListener('submit', e => {
+  e.preventDefault()
+
+  const title = noteTitle.value
+  const content = noteContent.value
+  
+  if (title == null || title == ' ') return
+  if (content == null || content == ' ') return;
+
+  const note = createNote(title, content)
+  notesList.push(note)
+  console.log(notesList);
+  
+  noteTitle.value = null
+  noteContent.value = null
+
+  save()
+  renderNotes()
+})
+
+/* function to create a new note object */
+function createNote(noteTitle, noteContent) {
+  return {
+      id: Date.now().toString(),
+      title: noteTitle,
+      content: noteContent
+  }
+}
+
+function clear(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild)
+  }
+}
+
+/* save everything to local storage */
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_SELECTED_TAB_ID_KEY, selectedTabId)
+  localStorage.setItem(LOCAL_STORAGE_SELECTED_NOTES_ID_KEY, JSON.stringify(notesList))
+}
+
+/* render data to the user interface */
+function render() {
+  sidebarList.forEach(tab => {
+      const pageId = tab.id + "-main"
+      const currentTab = document.getElementById(pageId)
+
+      if (tab.id == selectedTabId) {
+          tab.classList.add("clicked")
+          currentTab.classList.remove("hidden")
+      } else {
+          tab.classList.remove("clicked")
+          currentTab.classList.add("hidden")
+      }
+  })
+}
+
+function renderNotes() {
+  clear(notesContainer)  
+  notesList.forEach(note => {
+      const noteElement = document.importNode(noteTemplate.content, true)
+      const noteCard = noteElement.querySelector(".card")
+      const noteTitle = noteElement.querySelector(".card-title b")
+      const noteContent = noteElement.querySelector(".card-text")
+
+      noteCard.id = note.id
+      noteTitle.append(note.title)
+      noteContent.append(note.content)
+
+      notesContainer.prepend(noteElement)
+  })
+}
+
+render()
+renderNotes()
